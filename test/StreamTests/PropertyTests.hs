e6d0dc1bc32f3a9e18c38bb0959037c3e90b8edf
aske-design1 <hm64zk@student.aau.dk>
2026-04-04 18:12:19 +0200

    Property test is incorrect :(

--- test/StreamTests/PropertyTests.hs
+++ test/StreamTests/PropertyTests.hs
@@ -15,20 +15,23 @@ import Stream.Verdict (Verdict(FFalse))
 testStreamOutState = TestCase $
     let
         n = Val . VNum
-        expr = BinOp LessThan (n 50) (Val VTime)
+        expr = BinOp LessThan (Val VTime) (n 50) 
         prop = Prop Always None
 
         s = (M.! 0) . fst . convertCSTToStreams $ prop expr
 
         verdictsUnde = foldl (So.insert s) M.empty ([0..49] :: [Int])
 
-        updateFunc state t = So.update s state S.empty t 
+        updateFunc state = So.update s state S.empty
 
+        cleanUpState = (So.cleanUp s .) . updateFunc
     in
         assertEqual "Check Size of verdict" 50 (M.size verdictsUnde) >>
         assertEqual "Check whether all elements are undecided" True (all ((== Undecided) . snd) (toList verdictsUnde)) >>
-        assertEqual "Check whether all elements are undecided at time 49" True (all ((== Undecided) . snd) (toList (verdicts s))) >>
-        assertEqual "Check that all elements are false at time 50" True (all ((== FFalse) . snd) (M.toList $ updateFunc verdictsUnde 50))
+        assertEqual "Check whether all elements are undecided at time 49" True (all ((== Undecided) . snd) (toList verdictsUnde)) >>
+        assertEqual "Check that all elements are false at time 50" True (all ((== FFalse) . snd) (M.toList $ updateFunc verdictsUnde 50)) >>
+        assertEqual "Check Size after clean up at t = 49" 50 (M.size $ cleanUpState verdictsUnde 49) >>
+        assertEqual "Check Size after clean up at t = 50" 50 (M.size $ cleanUpState verdictsUnde 50)
 
 
 propertyTests :: Test
